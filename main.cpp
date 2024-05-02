b9f257cb43a490c68657077a61a81b0633287865
HeXueqiang <hexueqiang007@gmail.com>
2024-05-02 12:59:16 +0800

    成功的用ispc压缩出1.dds（BC7），但是astc中astcenccli_image_external.cpp又需要注释掉#define STB_IMAGE_IMPLEMENTATION这行
    TODO，不鼓励main中#include .cpp，整改整个cmake用法
    visual studio中调试变为了ori.png 1.dds   之前是1.astc

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 
 #include "astcenc.h"
 
-//#define STB_IMAGE_IMPLEMENTATION
-//#include "ThirdParty/stb_image.h"
-//
+#define STB_IMAGE_IMPLEMENTATION
+#include "ThirdParty/stb_image.h"
 //#define STB_IMAGE_WRITE_IMPLEMENTATION
 //#include "ThirdParty/stb_image_write.h"
 #include <thread>
@@ -12,11 +11,141 @@
 #include <astcenccli_internal.h>
 #include <astcenccli_toplevel.cpp>
 
+#include <ispc_texcomp.h>
+
 int CompressASTC(char** argv);
+
+
+
+
+
+//BC7的Header
+#include <stdint.h>
+#include <stdlib.h>
+#define MAKEFOURCC(ch0, ch1, ch2, ch3) \
+                ((uint32_t)(uint8_t)(ch0) | ((uint32_t)(uint8_t)(ch1) << 8) | \
+                ((uint32_t)(uint8_t)(ch2) << 16) | ((uint32_t)(uint8_t)(ch3) << 24 ))
+
+#define DDS_MAGIC 0x20534444  // "DDS "
+
+#define DDSD_CAPS 0x1
+#define DDSD_HEIGHT 0x2
+#define DDSD_WIDTH 0x4
+#define DDSD_PIXELFORMAT 0x1000
+#define DDSD_LINEARSIZE 0x80000
+
+#define DDPF_FOURCC 0x4
+
+#define DDSCAPS_TEXTURE 0x1000
+
+#define DXGI_FORMAT_BC7_UNORM 98
+#define D3D10_RESOURCE_DIMENSION_TEXTURE2D 3
+
+typedef struct {
+	uint32_t       dwSize;
+	uint32_t       dwFlags;
+	uint32_t       dwHeight;
+	uint32_t       dwWidth;
+	uint32_t       dwPitchOrLinearSize;
+	uint32_t       dwDepth;
+	uint32_t       dwMipMapCount;
+	uint32_t       dwReserved1[11];
+	struct {
+		uint32_t   dwSize;
+		uint32_t   dwFlags;
+		uint32_t   dwFourCC;
+		uint32_t   dwRGBBitCount;
+		uint32_t   dwRBitMask;
+		uint32_t   dwGBitMask;
+		uint32_t   dwBBitMask;
+		uint32_t   dwABitMask;
+	} ddpfPixelFormat;
+	struct {
+		uint32_t   dwCaps1;
+		uint32_t   dwCaps2;
+		uint32_t   dwDDSX;
+		uint32_t   dwReserved;
+	} ddsCaps;
+	uint32_t       dwReserved2;
+} DDS_HEADER;
+
+typedef struct {
+	uint32_t dxgiFormat;
+	uint32_t resourceDimension;
+	uint32_t miscFlag;
+	uint32_t arraySize;
+	uint32_t miscFlags2;
+} DDS_HEADER_DXT10;
+
+
+void write_dds(const char* filename, int width, int height, const void* data, size_t dataSize) {
+	FILE* file = fopen(filename, "wb");
+	if (!file) {
+		fprintf(stderr, "Failed to open output file\n");
+		return;
+	}
+
+	uint32_t ddsMagic = DDS_MAGIC;
+	fwrite(&ddsMagic, sizeof(uint32_t), 1, file);
+
+	DDS_HEADER header = { 0 };
+	header.dwSize = 124;
+	header.dwFlags = DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH | DDSD_PIXELFORMAT | DDSD_LINEARSIZE;
+	header.dwHeight = height;
+	header.dwWidth = width;
+	header.dwPitchOrLinearSize = dataSize;
+	header.dwDepth = 0;
+	header.dwMipMapCount = 0;
+	header.ddpfPixelFormat.dwSize = 32;
+	header.ddpfPixelFormat.dwFlags = DDPF_FOURCC;
+	header.ddpfPixelFormat.dwFourCC = MAKEFOURCC('D', 'X', '1', '0');
+	header.ddsCaps.dwCaps1 = DDSCAPS_TEXTURE;
+	fwrite(&header, sizeof(DDS_HEADER), 1, file);
+
+	DDS_HEADER_DXT10 dxt10Header = {
+		DXGI_FORMAT_BC7_UNORM,
+		D3D10_RESOURCE_DIMENSION_TEXTURE2D,
+		0,
+		1,
+		0
+	};
+	fwrite(&dxt10Header, sizeof(DDS_HEADER_DXT10), 1, file);
+	fwrite(data, 1, dataSize, file);
+	fclose(file);
+}
+
 
 int main(int argc, char** argv)
 {
-	return CompressASTC(argv);
+	//return CompressASTC(argv);
+
+	int image_x, image_y, image_c;
+	uint8_t* image_data = (uint8_t*)stbi_load(argv[1], &image_x, &image_y, &image_c, 4);
+
+	bc7_enc_settings encodingSettings;
+	GetProfile_basic(&encodingSettings);
+	//GetProfile_alpha_basic(&encodingSettings);
+
+	rgba_surface surfaceInput;
+	surfaceInput.ptr = image_data;
+	surfaceInput.stride = image_x * 4;
+	surfaceInput.width = image_x;
+	surfaceInput.height = image_y;
+
+	// Calculate buffer size for output, considering each block compresses to 16 bytes
+	size_t num_blocks_wide = (image_x + 3) / 4;
+	size_t num_blocks_high = (image_y + 3) / 4;
+	size_t output_buffer_size = num_blocks_wide * num_blocks_high * 16;
+	uint8_t* output_buffer = (uint8_t*)malloc(output_buffer_size);
+
+	//为什么单线程也能这么快？内部是多线程？
+	CompressBlocksBC7(&surfaceInput, output_buffer, &encodingSettings);
+
+	write_dds(argv[2], image_x, image_y, output_buffer, output_buffer_size);
+
+	free(output_buffer);
+	stbi_image_free(image_data);
+	return 1;
 }
 
 int CompressASTC(char** argv)
